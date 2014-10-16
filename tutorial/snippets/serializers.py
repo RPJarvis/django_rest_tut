f584500d449897da3fc01ad9d4e0ae65581f7693
RPJarvis <RJarvis1@cnm.edu>
2014-10-16 09:04:34 -0600

    first commit

---
+++ tutorial/snippets/serializers.py
@@ -0,0 +1,27 @@
+from django.forms import widgets
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class SnippetSerializer(serializers.Serializer):
+    class Meta:
+        model = Snippetfields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+
+    # def restore_object(self, attrs, instance=None):
+    #     """
+    #     create or update a new snippet instance, given dictionary of deserialized field values
+    #     without this method desrializing will simply return a dict of items
+    #     """
+    #
+    #     if instance:
+    #         #update it
+    #         instance.title = attrs.get('title', instance.title)
+    #         instance.code = attrs.get('code', instance.code)
+    #         instance.linenos = attrs.get('linenos', instance.linenos)
+    #         instance.language  = attrs.get('language', instance.language)
+    #         instance.style = attrs.get('style', instance.style)
+    #
+    #         return instance
+    #
+    #     #create new instance
+    #     return Snippet(**attrs)
